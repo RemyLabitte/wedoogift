a7affebd97014133e725e0098a73ac3de9bd20ee
Rémy Labitte <remy.labitte@ext.leroymerlin.fr>
2022-11-29 13:48:43 +0100

    🐛 Fix MealDepositController (Exceptions Management and method names)

--- src/main/java/com/backchallenge/wedoogift/Controllers/MealDepositController.java
+++ src/main/java/com/backchallenge/wedoogift/Controllers/MealDepositController.java
@@ -5,6 +5,13 @@ import com.backchallenge.wedoogift.Exceptions.NotEnoughException;
 import com.backchallenge.wedoogift.Exceptions.NotFoundException;
 import com.backchallenge.wedoogift.Services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +28,28 @@ public class MealDepositController {
     }
 
     @Operation(
-            summary = "Allow companies to do a gift deposit for a user")
+            summary = "Allow companies to do a meal deposit for a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "400", description = "Company has not enough to do a deposit",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = String.class)))
+    })
     @PostMapping
-    public String doAGiftDepositToUser(
-            @RequestBody MealDepositInput mealDeposit) throws NotEnoughException, NotFoundException {
-        userService.doAMealDeposit(mealDeposit);
-        return "Deposit done";
+    public ResponseEntity<String> doAMealDepositToUser(
+            @RequestBody MealDepositInput mealDeposit) {
+        try {
+            userService.doAMealDeposit(mealDeposit);
+            return ResponseEntity.ok("Deposit done");
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (NotEnoughException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
